d2ce0932f82b8626e51e3581c8ba7138a22c2e57
Алтай <altosha211@gmail.com>
2020-07-29 16:36:39 +0300

    Added Selenium, Beautiful Soup
    Needed to modify post searching

--- parse.py
+++ parse.py
@@ -1,14 +1,68 @@
+import time
+
+
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-from urllib.request import *
+from selenium import webdriver
 
-url = 'https://vk.com/@yvkurse-muzeinyi-kompleks-tolbuhino'
 
-def get_html():
-    request = Request(url)
-    html = urlopen(request).read()
-    return html
+browser = webdriver.Chrome(ChromeDriverManager().install())
 
-def main():
-    opener =
 
-main()
+def start_browser():
+
+    options = webdriver.ChromeOptions()
+    prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2,  # ОТКЛЮЧЕНИЕ КАРТИНОК И ТД
+                                                        'plugins': 2, 'popups': 2, 'geolocation': 2,
+                                                        'notifications': 2, 'auto_select_certificate': 2,
+                                                        'fullscreen': 2,
+                                                        'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+                                                        'media_stream_mic': 2, 'media_stream_camera': 2,
+                                                        'protocol_handlers': 2,
+                                                        'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2,
+                                                        'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                        'metro_switch_to_desktop': 2,
+                                                        'protected_media_identifier': 2, 'app_banner': 2,
+                                                        'site_engagement': 2,
+                                                        'durable_storage': 2}}
+    options.add_experimental_option('prefs', prefs)
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument('--headless')
+
+    browser.get("https://vk.com/@yvkurse-muzeinyi-kompleks-tolbuhino")
+
+
+def scroll_down():
+
+    scroll_pause_time = 1
+
+    last_height = browser.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(scroll_pause_time)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = browser.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+    return browser.page_source
+
+
+def parse_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    post = soup.find('div', class_='article article_view')
+    links = post.find_all('div', class_= 'image')
+    print([i.find('img')['src'] for i in links])
+    text = post.text
+    print(text)
+
+
+start_browser()
+parse_html(scroll_down())
